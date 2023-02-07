e721292a5e1f8922908e94c02c0d144ee6107126
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-07 23:55:51 +1100

    11. Updated Mapping

--- pizzeriaserver/pizzeriaserver/Mappings/MappingProfile.cs
+++ pizzeriaserver/pizzeriaserver/Mappings/MappingProfile.cs
@@ -12,14 +12,9 @@ namespace pizzeriaserver.Mappings
                 .ForMember(x => x.Name, s => s.MapFrom(source => source.Name))
                 .ForMember(x => x.Description, s => s.MapFrom(source => source.Description))
                 .ForMember(x => x.Price, s => s.MapFrom(source => source.Price))
-                .ForMember(x => x.Location, s => s.MapFrom(source => source.Location));
-
-            CreateMap<Pizza, PizzaDto>()
                 .ForMember(x => x.Id, s => s.MapFrom(source => source.Id))
-                .ForMember(x => x.Name, s => s.MapFrom(source => source.Name))
-                .ForMember(x => x.Description, s => s.MapFrom(source => source.Description))
-                .ForMember(x => x.Price, s => s.MapFrom(source => source.Price))
-                .ForMember(x => x.Location, s => s.MapFrom(source => source.Location));
+                .ForMember(x => x.Location, s => s.MapFrom(source => source.Location))
+                .ReverseMap();
         }
     }
 }
