086d47f5e9b79e3ee633714d1fc0f8bef41b3b0f
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-07 16:26:28 +1100

    1. Created new Web API Project and added PizzaController and Pizza.cs model

---
+++ pizzeriaserver/pizzeriaserver/Controllers/PizzaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using pizzeriaserver.Models;
+
+namespace pizzeriaserver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public List<Pizza> GetAll()
+        {
+            return new List<Pizza>();
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok(new Pizza() { Id = id });
+        }
+    }
+}
---
+++ pizzeriaserver/pizzeriaserver/Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace pizzeriaserver.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Location { get; set; }
+    }
+}
