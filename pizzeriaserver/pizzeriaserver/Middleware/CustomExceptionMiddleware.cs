b6b106766012602b2abc54be5e10c39d0054d66f
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-10 00:19:45 +1100

    23. Returning serialized response to custom middleware

--- pizzeriaserver/pizzeriaserver/Middleware/CustomExceptionMiddleware.cs
+++ pizzeriaserver/pizzeriaserver/Middleware/CustomExceptionMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿using pizzeriaserver.Application.Common.Exceptions;
 using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace pizzeriaserver.Middleware
 {
@@ -20,24 +22,41 @@ namespace pizzeriaserver.Middleware
             }
             catch (NotFoundException ex)
             {
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                await context.Response.WriteAsync(ex.Message);
+                var result = SerializeObject(new[] { ex.Message });
+                await context.Response.WriteAsync(result);
             }
             catch (BadRequestException ex)
             {
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                await context.Response.WriteAsync(ex.Message);
+                var result = SerializeObject(new[] { ex.Message });
+                await context.Response.WriteAsync(result);
             }
             catch (DuplicateItemException ex)
             {
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.Conflict;
-                await context.Response.WriteAsync(ex.Message);
+                var result = SerializeObject(new[] { ex.Message });
+                await context.Response.WriteAsync(result);
             }
             catch (ValidationException ex)
             {
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                await context.Response.WriteAsync(ex.Message);
+                var result = SerializeObject(new[] { ex.Message });
+                await context.Response.WriteAsync(result);
             }
         }
+
+        private static string SerializeObject(object obj)
+            => JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy(true, true)
+                }
+            });
     }
 }
