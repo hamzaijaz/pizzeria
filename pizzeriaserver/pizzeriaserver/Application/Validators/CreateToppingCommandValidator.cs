9facfa31cf14de28888d92e0b370b2827f9a34f0
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-10 02:08:56 +1100

    28. Added CreateToppingValidator

---
+++ pizzeriaserver/pizzeriaserver/Application/Validators/CreateToppingCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using pizzeriaserver.Application.Commands;
+
+namespace pizzeriaserver.Application.Validators
+{
+    public class CreateToppingCommandValidator : AbstractValidator<CreateToppingCommand>
+    {
+        public CreateToppingCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotNull().WithMessage("Name must not be null")
+                .NotEmpty().WithMessage("Name must not be empty");
+        }
+    }
+}
