d32c56c75fddfddeb5a144906145d8b1ca4c5b20
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-10 16:46:56 +1100

    34. Added Validators for some queries

---
+++ pizzeriaserver/pizzeriaserver/Application/Validators/GetPizzaByIdQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using pizzeriaserver.Application.Queries;
+
+namespace pizzeriaserver.Application.Validators
+{
+    public class GetPizzaByIdQueryValidator : AbstractValidator<GetPizzaByIdQuery>
+    {
+        public GetPizzaByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotNull().WithMessage("Pizza Id must not be null")
+                .NotEmpty().WithMessage("Pizza Id must not be empty")
+                .GreaterThan(0).WithMessage("Pizza Id must be greater than zero");
+        }
+    }
+}
---
+++ pizzeriaserver/pizzeriaserver/Application/Validators/GetToppingByIdQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using pizzeriaserver.Application.Queries;
+
+namespace pizzeriaserver.Application.Validators
+{
+    public class GetToppingByIdQueryValidator : AbstractValidator<GetToppingByIdQuery>
+    {
+        public GetToppingByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotNull().WithMessage("Topping Id must not be null")
+                .NotEmpty().WithMessage("Topping Id must not be empty")
+                .GreaterThan(0).WithMessage("Topping Id must be greater than zero");
+        }
+    }
+}
