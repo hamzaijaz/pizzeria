15a23d609a39960136355b21abc0c512f98cfc25
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-02-14 06:40:18 +1100

    76. Added more tests

---
+++ pizzeriaserver/pizzeriaserver.IntegrationTests/ApplicationTests/Commands/CreateLocationCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using pizzeriaserver.Application.Commands;
+using pizzeriaserver.Application.Models;
+using pizzeriaserver.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzeriaserver.IntegrationTests.ApplicationTests.Commands
+{
+    [Collection("Tests")] // XUnit Requirement
+    public class CreateLocationCommandTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IAdminRepository> _mockAdminRepository;
+        private readonly CreateLocationCommand.CreateLocationCommandHandler _handler;
+
+        public CreateLocationCommandTests()
+        {
+            _fixture = new Fixture();
+            _mockAdminRepository = new Mock<IAdminRepository>();
+            _handler = new CreateLocationCommand.CreateLocationCommandHandler(_mockAdminRepository.Object);
+        }
+
+        [Fact]
+        public async Task CreateLocationCommand_ShouldAddLocationAndReturnLocationDto()
+        {
+            // Arrange
+            var locationDetails = _fixture.Create<LocationDto>();
+            _mockAdminRepository
+                .Setup(x => x.AddLocationAsync(It.IsAny<LocationDto>()))
+                .ReturnsAsync(locationDetails);
+
+            // Act
+            var result = await _handler.Handle(
+                new CreateLocationCommand { Name = locationDetails.Name, Address = locationDetails.Address },
+                CancellationToken.None);
+
+            // Assert
+            result.Should().BeEquivalentTo(locationDetails);
+        }
+    }
+}
---
+++ pizzeriaserver/pizzeriaserver.IntegrationTests/ApplicationTests/Validators/CreatePizzaCommandValidatorTests.cs
@@ -0,0 +1,162 @@
+﻿using FluentAssertions;
+using pizzeriaserver.Application.Commands;
+using pizzeriaserver.Application.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzeriaserver.IntegrationTests.ApplicationTests.Validators
+{
+    [Collection("Tests")]
+    public class CreatePizzaCommandValidatorTests
+    {
+        private readonly CreatePizzaCommandValidator _validator;
+
+        public CreatePizzaCommandValidatorTests()
+        {
+            _validator = new CreatePizzaCommandValidator();
+        }
+
+        [Fact]
+        public void When_Name_Is_Null_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = null,
+                Description = "Delicious Pizza",
+                Price = 10.99m,
+                LocationIds = new List<int> { 1, 2, 3 }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Name must not be null");
+        }
+
+        [Fact]
+        public void When_Name_Is_Empty_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = string.Empty,
+                Description = "Delicious Pizza",
+                Price = 10.99m,
+                LocationIds = new List<int> { 1, 2, 3 }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Name must not be empty");
+        }
+
+        [Fact]
+        public void When_Description_Is_Null_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = "Margherita",
+                Description = null,
+                Price = 10.99m,
+                LocationIds = new List<int> { 1, 2, 3 }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Pizza description must not be null");
+        }
+
+        [Fact]
+        public void When_Description_Is_Empty_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = "Margherita",
+                Description = string.Empty,
+                Price = 10.99m,
+                LocationIds = new List<int> { 1, 2, 3 }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Pizza description must not be empty");
+        }
+
+        [Fact]
+        public void When_Price_Is_LessThanZero_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = "Margherita",
+                Description = "Delicious Pizza",
+                Price = -20m,
+                LocationIds = new List<int> { 1, 2, 3 }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Pizza price must be greater than or equal to zero");
+        }
+
+        [Fact]
+        public void When_LocationIds_Is_Null_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = "Margherita",
+                Description = "Delicious Pizza",
+                Price = 20m,
+                LocationIds = null
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Locations list must not be null");
+        }
+
+        [Fact]
+        public void When_LocationIds_Is_Empty_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreatePizzaCommand
+            {
+                Name = "Margherita",
+                Description = "Delicious Pizza",
+                Price = 20m,
+                LocationIds = new List<int>()
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(error => error.ErrorMessage == "Please select atleast one locaiton");
+        }
+    }
+}
